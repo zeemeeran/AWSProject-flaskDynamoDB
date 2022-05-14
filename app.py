ae3ca543028c5e6642b87582b09bd60c6375ced1
Zahida Meeran <dev.zmeeran@gmail.com>
2022-05-14 17:24:23 -0400

    Zee adding test app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from re import T
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, again again  World!</p>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
